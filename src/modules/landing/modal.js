c6d5cb8ff8e50a6da2ad2811d2fc33b634c61bc0
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-05-12 17:10:02 +0400

    modal consultation add

--- src/modules/landing/modal.js
+++ src/modules/landing/modal.js
@@ -1,15 +1,62 @@
 export const modal = () => {
   const priceLinks = document.querySelectorAll('.link-list > a');
-  const price = document.querySelector('.popup-repair-types');
-  const closeBtn = price.querySelector('.close ')
+  const priceModal = document.querySelector('.popup-repair-types');
+  const closePriceModalBtn = priceModal.querySelector('.close');
+  const privacyLinks = document.querySelectorAll('.link-privacy');
+  const privacyModal = document.querySelector('.popup-privacy');
+  const closePrivacyModalBtn = privacyModal.querySelector('.close');
+  const transparencyLinks = document.querySelectorAll('.transparency-item__img');
+  const transparencyModal = document.querySelector('.popup-transparency');
+  const closeTransparencyModalBtn = transparencyModal.querySelector('.close');
+  const consultationLink = document.querySelectorAll('.button_wide');
+  const consultationModal = document.querySelector('.popup-consultation');
+  const closeconsultationModalBtn = consultationModal.querySelector('.close');
 
-  priceLinks.forEach(link => {
+  const openModal = (modal) => {
+    modal.style.visibility = 'visible';
+  }
+
+  const closeModal = (modal) => {
+    modal.style.visibility = 'hidden';
+  }
+
+  privacyLinks.forEach(link => {
     link.addEventListener('click', () => {
-      price.style.visibility = 'visible';
+      openModal(privacyModal)
     })
   })
 
-  closeBtn.addEventListener('click', () => {
-    price.style.visibility = 'hidden';
+  priceLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      openModal(priceModal)
+    })
+  })
+
+  transparencyLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      openModal(transparencyModal)
+    })
+  })
+
+  consultationLink.forEach(link => {
+    link.addEventListener('click', () => {
+      openModal(consultationModal)
+    })
+  })
+
+  closePriceModalBtn.addEventListener('click', () => {
+    closeModal(priceModal)
+  })
+
+  closePrivacyModalBtn.addEventListener('click', () => {
+    closeModal(privacyModal)
+  })
+
+  closeTransparencyModalBtn.addEventListener('click', () => {
+    closeModal(transparencyModal)
+  })
+
+  closeconsultationModalBtn.addEventListener('click', () => {
+    closeModal(consultationModal)
   })
 }
