3528e0dcbc9a1acf442fac18d30a1947016963db
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-05-14 18:47:14 +0400

    admin-login

---
+++ src/admin.js
@@ -0,0 +1,3 @@
+const { login } = require("./modules/admin-panel/login");
+
+login()
---
+++ src/modules/admin-panel/login.js
@@ -0,0 +1,60 @@
+export const login = () => {
+  const form = document.querySelector('form');
+  const inputName = document.getElementById('name');
+  const inputPass = document.getElementById('type');
+  const messageName = document.querySelector('.text-warning-name');
+  const messagePass = document.querySelector('.text-warning-password');
+
+  const checkAuth = () => {
+    if (window.location.pathname === '/index.html') {
+      localStorage.removeItem('auth');
+    }
+
+    if (window.location.pathname === '/table.html') {
+      if (!localStorage.getItem('auth')) {
+        window.location.href = `/index.html`;
+      }
+    }
+  }
+
+  const addCookie = () => {
+    let date = new Date(2022, 6, 4,);
+    document.cookie = `name=${inputName.value}; expires=${date.toUTCString()};`
+  }
+
+  const authorization = (users) => {
+    messageName.style.display = 'none';
+    messagePass.style.display = 'none';
+
+    users.forEach(user => {
+      if (inputName.value === user.login && inputPass.value === user.password) {
+        localStorage.setItem('auth', JSON.stringify({ auth: true }));
+        addCookie();
+
+        window.location.href = `/table.html`;
+      } else {
+        if (inputName.value !== user.login) {
+          messageName.style.display = 'block';
+        } else if (inputPass.value !== user.password) {
+          messagePass.style.display = 'block';
+        }
+
+        inputName.value = '';
+        inputPass.value = '';
+      }
+    })
+  }
+
+  const sendRequest = () => {
+    return fetch('http://localhost:4444/users')
+      .then(res => res.json())
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendRequest().then(users => authorization(users));
+  })
+
+  checkAuth();
+
+}
